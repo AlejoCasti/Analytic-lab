65e63f5dec06abdd3a3b0bf1b9c405c1be503539
AlejoCasti <acastiblanco8@gmail.com>
2020-07-12 20:39:07 -0500

    [GET]  create server to run

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static('./dist/analytic-lab'));
 app.get('/*', function(req, res) {
-    res.sendFile('./src/index.html', {root: 'dist/analytic-lab/'}
+    res.sendFile('/index.html', {root: 'dist/analytic-lab/'}
   );
 });
 app.listen(process.env.PORT || 8080);
