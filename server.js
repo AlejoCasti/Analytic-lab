3182a24c642c8cc92c9884622f5778537c355bd9
AlejoCasti <acastiblanco8@gmail.com>
2020-07-12 20:57:09 -0500

    [FIX] path of dist

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('./dist/analytic-lab'));
+app.use(express.static('./dist/Analytic-lab'));
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/analytic-lab/'}
+    res.sendFile('index.html', {root: 'dist/Analytic-lab/'}
   );
 });
 app.listen(process.env.PORT || 8080);
